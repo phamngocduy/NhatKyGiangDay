734cdee0f9d26ffaf8bf125bc1e0bd45589b6df5
Pham Ngoc Duy <duy.pn@vlu.edu.vn>
2021-11-02 15:18:11 +0700

    Empty project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NhatKyGiangDay.Startup))]
+namespace NhatKyGiangDay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
