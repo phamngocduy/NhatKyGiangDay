2a1bfb8e12df3865942d970c5934c43c5f098795
Pham Ngoc Duy <duy.pn@vlu.edu.vn>
2021-11-02 15:08:41 +0700

    Empty project

---
+++ HackingOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HackingOnline.Startup))]
+namespace HackingOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
